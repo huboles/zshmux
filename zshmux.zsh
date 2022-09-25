0bdb34bff5e971aef842bc7c90de66a5f9eb323c
Huck Boles <huboles@protonmail.com>
2022-09-24 20:03:27 -0500

    Vim quick push

--- zshmux.zsh
+++ zshmux.zsh
@@ -1,16 +1,16 @@
 #!/bin/zsh
 
 if [[ -z $TMUX ]]; then
-    if tlist &> /dev/null ; then
-        printf 'Current tmux sessions:\n\n'
-        tlist
+    if tmux list-sessions &> /dev/null ; then
+        printf 'Current tmux sessions:\n'
+        tmux list-sessions -F"#S - (#{?session_attached,Attached,Not Attached})"
     else
         printf '\nNo tmux sessions active\n'
     fi
-    printf '\n\tAttach session?: [name] (empty if none): '
+    printf 'Attach session?: [name] (empty if none): '
     read response
     if [ -n "$response" ]; then
-        if tlist | grep -q "$response"; then
+        if tmux list-sessions | grep -q "$response"; then
             tmux attach-session -t $response 
         else
             tmux new-session -s $response
