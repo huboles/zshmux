8c44b3f84cd37beb1d0f0d720a9dd1c06bb7df10
Huck Boles <huboles@protonmail.com>
2022-09-24 21:27:35 -0500

    Vim quick push

--- zshmux.zsh
+++ zshmux.zsh
@@ -5,7 +5,7 @@ if [[ -z $TMUX ]]; then
         printf '\033[32;1mCurrent \033[33;1mtmux \033[32;1msessions\033[0m:\n'
         tmux list-sessions -F"#S - (#{?session_attached,Attached,Not Attached})"
     else
-        printf '\n\033[34;1mNo \033[33;1mtmux \033[34;1msessions active\n'
+        printf '\033[34;1mNo \033[33;1mtmux \033[34;1msessions active\n'
     fi
     printf '\033[34;1mAttach session?\033[0;m: [\033[35;1mname\033[0m] \033[37;2m(empty if none)\033[0m: '
     read session
