9296f987d22b905f403fa2783b908d164225a9f6
Huck Boles <huck@huck.website>
2023-05-13 19:51:28 -0500

    edit look

--- zshmux.sh
+++ zshmux.sh
@@ -12,13 +12,13 @@ if [[ $(tmux list-sessions | wc -l) -ne 0 ]]; then
         tmux attach-session -t $(tmux list-sessions -F"#{?session_attached,,#S}"  )
         return
     else
-        printf '\033[32;1mCurrent \033[33;1mtmux \033[32;1msessions\033[0m:\n'
-        tmux list-sessions -F"#S - (#{?session_attached,Attached,Not Attached})"
-        printf '\033[34;1mAttach session?\033[0;m: [\033[35;1mname\033[0m] \033[37;2m(empty if none)\033[0m: '
+        printf '\033[32;1mcurrent \033[33;1mtmux \033[32;1msessions\033[0m:\n'
+        tmux list-sessions -F"#S - (#{?session_attached,attached,not attached})"
+        printf '\033[34;1mattach session?\033[0;m: [\033[35;1mname\033[0m] \033[37;2m(empty if none)\033[0m: '
     fi
 else
-    printf '\033[34;1mNo \033[33;1mtmux \033[34;1msessions active\n'
-    printf '\033[34;1mCreate new session?\033[0;m: [\033[35;1mname\033[0m] \033[37;2m(empty if none)\033[0m: '
+    printf '\033[34;1mno \033[33;1mtmux \033[34;1msessions active\n'
+    printf '\033[34;1mcreate new session?\033[0;m: [\033[35;1mname\033[0m] \033[37;2m(empty if none)\033[0m: '
 fi
 
 read session
